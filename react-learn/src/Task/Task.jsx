ffef49c1599d3f66797b06fbe1ea15f2b214cf38
jasonenkristo123 <jasonenkristo@gmail.com>
2026-01-13 16:41:25 +0700

    refactor code

--- react-learn/src/Task/Task.jsx
+++ react-learn/src/Task/Task.jsx
@@ -1,37 +1,19 @@
-import { useState } from "react";
-import { renderToReadableStream } from "react-dom/server";
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 export default function Task() {
-    const [item, setItem] = useState("");
     const [items, setItems] = useImmer([]);
 
-    function handleChange(e) {
-        setItem(e.target.value);
-    }
-
-    function handleClick(e) {
-        e.preventDefault();
+    function handleSubmit(item) {
         setItems(draft => {
             draft.push(item);
         })
-
-        setItem("");
     }
-
     return(
         <div>
-            <h1>Create Task</h1>
-            <form>
-                <input type="text" value={item} onChange={handleChange}/>
-                <button onClick={handleClick}>Add</button>
-            </form>
-            <h1>List Task</h1>
-            <ul>
-                {items.map((item, index) => 
-                    <li key={index}>{item}</li>
-                )}
-            </ul>
+            <TaskForm onSubmit= {handleSubmit} />
+            <TaskList items = {items} />
         </div>
     )
 
