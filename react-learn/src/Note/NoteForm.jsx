bb088ce03f39b79f86b3e61625becaf06c1c93ea
jasonenkristo123 <jasonenkristo@gmail.com>
2026-01-20 13:42:25 +0700

    usememo

--- react-learn/src/Note/NoteForm.jsx
+++ react-learn/src/Note/NoteForm.jsx
@@ -1,10 +1,18 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { NotesDispatchContext } from "./NoteContext";
 
 
 export default function NoteForm() {
     const [text, setText] = useState("");
     const dispatch = useContext(NotesDispatchContext);
+    const noteInput = useRef(null);
+
+    function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            handleClick();
+        }
+    }
 
     function handleChange(e) {
         setText(e.target.value);
@@ -16,11 +24,12 @@ export default function NoteForm() {
             type: "ADD_NOTE",
             text: text
         })
+        noteInput.current.focus();
     }
 
     return (
         <>
-            <input placeholder="Add Note" onChange={handleChange} value={text} />
+            <input ref={noteInput} placeholder="Add Note" onChange={handleChange} value={text} onKeyDown={handleKeyDown} />
             <button onClick={handleClick}>Add</button>
         </>
     )
