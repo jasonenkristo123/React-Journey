8d7f142f16e358c65e1943bac4057934633cc779
jasonenkristo123 <jasonenkristo@gmail.com>
2026-01-20 12:04:36 +0700

    dependencies

--- react-learn/src/product/productList.jsx
+++ react-learn/src/product/productList.jsx
@@ -1,22 +1,31 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./product";
 import '../index.css'
 
 export default function ProductList() {
     const [products, setProduct] = useState([]);
-    const isLoaded = useRef(false);
+    const [load, setLoad] = useState(false);
 
     useEffect(() => {
-        if (isLoaded.current === false) {
+        if (load) {
             fetch("/products.json")
                 .then((response) => response.json())
                 .then((data) => setProduct(data))
-                .then(() => isLoaded.current = true)
         }
-    }, [])
+
+        return () => {
+            console.info("clean up");
+        }
+    }, [load])
+    
+    function handleClick() {
+        setLoad(true);
+    }
 
     return (
         <div className="flex flex-row flex-wrap gap-6 m-6">
+            <h1>Product List</h1>
+            <button onClick={handleClick}>Load Product</button>
             {products.map((product) => (
                 <Product key={product.id} product={product} />
             ))}
